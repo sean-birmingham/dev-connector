49e7fe122981a817e498eb800c76261b883850ce
Sean Birmingham <seannbirmingham98@gmail.com>
2020-05-19 14:54:04 -0400

    set up route to add a post

--- models/Post.js
+++ models/Post.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const PostSchema = new Schema({
+const PostSchema = new mongoose.Schema({
   user: {
     type: mongoose.Schema.Types.ObjectId,
     ref: 'user',
